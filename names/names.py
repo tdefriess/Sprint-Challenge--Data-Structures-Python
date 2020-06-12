f18f546e5494a80fec8ff67c7de0f90834a4d2c8
Tyler de Friess <tyler.defriess@gmail.com>
2020-06-12 12:24:02 -0400

    Names.py runtime reduced from 4.7 to 0.87 seconds.

--- names/names.py
+++ names/names.py
@@ -13,10 +13,58 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):        
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value and self.left is not None:
+            return self.left.contains(target)
+        if target > self.value and self.right is not None:
+            return self.right.contains(target)
+        else:
+            return False
+
+    def find_duplicates(self):
+        if self.right is not None and self.value == self.right.value:
+            duplicates.append(self.value)
+
+        
+
+        if self.left is not None:
+            self.left.find_duplicates()
+
+        if self.right is not None:
+            self.right.find_duplicates()
+
+bst = BSTNode(names_1[0])
+for name in names_1[1:]:
+    bst.insert(name)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+bst.find_duplicates()
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
